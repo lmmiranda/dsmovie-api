03cf0af379d6a078e1066fa8cda795b74c26856b
lmmiranda <lmmiranda412@gmail.com>
2022-01-10 18:06:41 -0300

    Project base

---
+++ src/main/java/com/devsuperior/dsmovieapi/DsmovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsmovieapi/DsmovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
